62c53914a0aec1b6075bdae905886fd3c0358384
Michael Curtis <michael@Michaels-MacBook-Air.local>
2024-01-26 22:06:02 -0500

    Update

--- lisp/init-code.el
+++ lisp/init-code.el
@@ -15,5 +15,18 @@
   :defer t
   :diminish
   :init (global-flycheck-mode))
+(use-package lsp-mode
+  :straight t
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (XXX-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+(use-package lsp-ivy
+  :straight t
+  :commands lsp-ivy-workspace-symbol)
 
 (provide 'init-code)
--- lisp/init-navigation.el
+++ lisp/init-navigation.el
@@ -1,11 +1,12 @@
-(use-package centaur-tabs
-  :straight t
-  :demand
-  :config
-  (setq centaur-tabs-style "slant")
-  (setq centaur-tabs-set-icons t)
-  (centaur-tabs-mode t)
-  :bind
-  ("C-<prior>" . centaur-tabs-backward)
-  ("C-<next>" . centaur-tabs-forward))
+;;(use-package centaur-tabs
+;;  :straight t
+;;  :demand
+;;  :config
+;;  (setq centaur-tabs-style "slant")
+;;  (setq centaur-tabs-set-icons t)
+;;  (centaur-tabs-mode t)
+;;  :bind
+;;  ("C-<prior>" . centaur-tabs-backward)
+;;  ("C-<next>" . centaur-tabs-forward))
+
 (provide 'init-navigation)
