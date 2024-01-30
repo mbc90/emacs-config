e9db90bbfd59ac9f2bc63edea93e179da1cf8da5
Michael Curtis <michael@Michaels-MacBook-Air.local>
2024-01-30 13:16:31 -0500

    y

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -6,14 +6,14 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-gruvbox t)
+  (load-theme 'doom-tokyo-night t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (setq doom-themes-treemacs-theme "doom-colors") ; use "doom-colors" for less minimal icon theme
   (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
@@ -31,5 +31,12 @@
 (set-fringe-mode 10)        ; Give some breathing room
 
 (menu-bar-mode -1)            ; Disable the menu bar
+
+;; use-package with package.el:
+(use-package dashboard
+  :straight t
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook))
 
 (provide 'init-themes)
