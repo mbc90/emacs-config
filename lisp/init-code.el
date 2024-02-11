6e84886c962ff91730ebc6bf96064a1f3becd18c
Michael C <michaelbc71@protonmail.com>
2024-02-11 14:56:23 -0500

    modified gitignore

--- lisp/init-code.el
+++ lisp/init-code.el
@@ -21,12 +21,25 @@
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   (setq lsp-keymap-prefix "C-c l")
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         (XXX-mode . lsp)
+
+	 ;; c lsp lang
+         (c-mode . lsp)
+         (c++-mode . lsp)
          ;; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
 (use-package lsp-ivy
   :straight t
   :commands lsp-ivy-workspace-symbol)
+
+;; gives me pretty auto complete boxes
+(use-package company
+  :straight t
+  :after lsp-mode
+  :hook (prog-mode . company-mode)
+  :bind (:map company-active-map
+	      ("<tab>" . company-complete-selection))
+  (:map lsp-mode-map
+	      ("<tab>" . company-indent-or-complete-common)))
 
 (provide 'init-code)
